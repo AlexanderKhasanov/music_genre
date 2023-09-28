c6f384d086e21bcbbeccdddd94ee4ff1fd7a9e0a
Александр Хасанов <AlexanderKhasanov1205@yandex.ru>
2023-09-28 16:30:05 +0300

    fix

--- create_venv_linux.py
+++ create_venv_linux.py
@@ -2,5 +2,5 @@ import os
 
 os.system(('python3.9 -m venv venv && '
            '. venv/bin/activate && '
-           'pip install -r requirements.txt && '
+           'pip install -r requirements_linux.txt && '
            'ipython kernel install --user --name=venv_kernel'))
